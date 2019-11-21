a5737a2491af623582f62ea33ea0e14c7a91b0cf
MTH <coo500559@gmail.com>
2019-11-21 12:48:24 +0900

    11M21D

--- 2019-11-19.sql
+++ 2019-11-19.sql
@@ -1,12 +1,8 @@
-SELECT *
-FROM lprod;
+SELECT * FROM lprod;
 
-SELECT buyer_id, buyer_name
-FROM buyer;
+SELECT buyer_id, buyer_name FROM buyer;
 
-SELECT *
-FROM cart;
+SELECT * FROM cart;
 
-SELECT mem_id, mem_pass, mem_name
-FROM member;
+SELECT mem_id, mem_pass, mem_name FROM member;
 
